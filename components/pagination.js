7b39920ca1340759a3a0c9a6d045c32094a2dc35
n4bc <fortheworkingprogress@gmail.com>
2023-09-20 22:43:14 +0300

    feat: pagination buttons added

--- components/pagination.js
+++ components/pagination.js
@@ -7,8 +7,9 @@ export function pagination() {
 
    async function main() {
       const postsData = await getData()
-      let currentPage = 1, rows = 10;
+      let currentPage = 1, rows = 11;
       function displayList(arrData, rowPerPage, page) {
+         page--
          const cardsEl = document.querySelector('[data-id="products"]'),
             start = rowPerPage * page,
             end = rowPerPage + start,
@@ -23,15 +24,16 @@ export function pagination() {
                src= ${el.src}
                alt="${el.name}">
          </div >
-         <h3 class="card__title">${el.name}</h3>
+         <a href="#" class="card__title" title="${el.name}">${el.name}</a>
          <div class="card-content">
             <p class="card__price">As low as <strong>${el.price}</strong></p>
             <button class="card__button button uppercase">
+            <img class="card__button-image" src="../image/collections/add-button.svg" alt="shopping bag logos">
                <span class="uppercase">ADD TO CART</span>
             </button>
          </div>
             `
-            cardsEl.appendChild(cardEl)
+            cardsEl.prepend(cardEl)
          })
 
             ;
@@ -39,10 +41,29 @@ export function pagination() {
       }
       function displayPagination(arrData, rowPerPage) {
          const paginationEl = document.querySelector(".pagination");
-         const pagesCount = Math.ceil(arrData.lenght / rowPerPage)
+         const pagesCount = Math.ceil(arrData.length / rowPerPage);
+         const ulEl = document.createElement('ul')
+         ulEl.classList.add('pagination__list')
+
+         for (let i = 0; i < pagesCount; i++) {
+            const liEl = displayPaginationBtn(i + 1)
+            ulEl.appendChild(liEl)
+         }
+         paginationEl.appendChild(ulEl)
       }
-      function displayPaginationBtn() { }
+      function displayPaginationBtn(page) {
+         const liEl = document.createElement('li')
+         liEl.classList.add('pagination__item')
+         liEl.innerText = page
+         liEl.addEventListener("click", () => {
+            currentPage = page
+            displayList(postsData, rows, currentPage)
+         })
+
+         return liEl
+      }
       displayList(postsData, rows, currentPage)
+      displayPagination(postsData, rows)
    }
 
 
